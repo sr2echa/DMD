36bc3d9baece3ea84a004f8562bdd83749ab04f4
Girish Raghav <119794177+wreckage0907@users.noreply.github.com>
2023-11-04 04:03:46 +0530

    latest

---
+++ src/detect.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from keras.models import load_model
+def detect(wallet):
+    response = requests.get(f'https://dirtyapi.replit.app/api/{wallet}')
+    dtf1 = response.json()
+    del dtf1["ERC20MostSentTokenType"]
+    del dtf1["ERC20MostRecTokenType"]
+    df1 = pd.DataFrame([dtf1])  
+    if 'Address' in df1.columns:
+        df1.drop(['Address'], axis=1, inplace=True)
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(df1)
+    loaded_model=load_model(r'/content/DMD.h5')
+    predictions1 = loaded_model.predict(df_scaled)
+    return predictions1 >=3.84e-05

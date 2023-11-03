c4df84c48ae249bf3f9ecbf00b8ad06b664fff83
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2023-11-03 15:34:38 +0530

    Update detector.py

--- web/detector.py
+++ web/detector.py
@@ -1,22 +1,34 @@
 import streamlit as st
 import subprocess
 import pandas as pd
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash import Input, Output
+
 df=pd.read_csv("./dataset.csv")
 df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 df2=df[['Address', 'Sent tnx','Received Tnx','Number of Created Contracts','total Ether sent','total ether received','total ether balance','FLAG']]
 
-# Set the page title and favicon
+
 st.set_page_config(
     page_title="DMD",
     page_icon=":bar_chart:",
-    layout="wide",
+    layout="wide"
 )
+app = dash.Dash(__name__)
 
-# Create a container for the header with a black background
+
+
 header_container = st.container()
 header_container.markdown(
     """
     <style>
+    @import url('https://fonts.googleapis.com/css?family=Fira+Code:400,500,600,700&display=swap');
+    .fira-code-font {
+        font-family: 'Fira Code', monospace;
+    }
     .header-container {
         background-color: #000000;
         color: #ffffff;
@@ -48,6 +60,17 @@ header_container.markdown(
         color: #ffffff;
         font-size: 18px;
         margin-bottom: 10px;
+    
+    }
+        .dirt-score {
+        color: #ffffff;
+        font-size: 18px;
+        margin-bottom: 10px;
+    }
+    .center-content {
+        text-align: center;
+        font-size: 24px;
+        margin-bottom: 20px;
     }
     </style>
     """,
@@ -55,12 +78,12 @@ header_container.markdown(
 )
 
 
-# Add the navigation links with white text to the right side
+
 with header_container:
-# Create a row
+
     col1,col2,col3,col4=st.columns([1,1,1,1])
 
-# Add a button to the row
+
     with col1:
         button1 = st.button("../")
 
@@ -77,14 +100,25 @@ st.markdown(vert_space, unsafe_allow_html=True)
 
 
 
-# Input text box with custom style
+
 wallet_address = st.text_input("Wallet Address", key="wallet_input", value="")
+percentage = 95  # You can change this percentage value
+
+# Set the text color based on the percentage
+if percentage > 65:
+    text_color = 'red'
+else:
+    text_color = 'green'
+
 
 
 if wallet_address:
     if wallet_address in df2['Address'].unique():
         st.write(f"Entered Wallet Address: {wallet_address}")
-        
+        st.markdown('<div class="fira-code-font" style="text-align: center; font-size: 20px;">Dirt Score</div>', unsafe_allow_html=True)
+        st.write(f"<div class='center-content fira-code-font' style='color: {text_color}; font-size: 36px;'><b>{percentage}</b></div>", unsafe_allow_html=True)
+
+
         df3 = df2.loc[df2['Address'] == wallet_address] 
         st.write(df3)
     else:

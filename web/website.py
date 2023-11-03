1203a44ba4f7e89ad11016497c0033d5f953e41c
J M Tarun <82860323+jmt-genius@users.noreply.github.com>
2023-11-04 03:55:03 +0530

    WEB_1

--- web/website.py
+++ web/website.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import subprocess
+import pandas as pd
+import plotly.graph_objects as go
+import dash
+
+df=pd.read_csv("./dataset.csv")
+df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+df2=df[['Address', 'Sent tnx','Received Tnx','Number of Created Contracts','total Ether sent','total ether received','total ether balance','FLAG']]
+
 
 
 # Set the page title and favicon
@@ -14,6 +22,10 @@ header_container = st.container()
 header_container.markdown(
     """
     <style>
+    @import url('https://fonts.googleapis.com/css?family=Fira+Code:400,500,600,700&display=swap');
+    .fira-code-font {
+        font-family: 'Fira Code', monospace;
+    }
     .header-container {
         background-color: #000000;
         color: #ffffff;
@@ -45,6 +57,25 @@ header_container.markdown(
         color: #ffffff;
         font-size: 18px;
         margin-bottom: 10px;
+
+    }
+    .dirt-score {
+        color: #ffffff;
+        font-size: 18px;
+        margin-bottom: 10px;
+    }
+    .center-content {
+        text-align: center;
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+    .sidebar .css-14ryutq {
+        font-family: 'Fira Code', monospace;
+    }
+
+    /* Specify the font for the content */
+    .content .css-vfskoc {
+        font-family: 'Fira Code', monospace;
     }
     </style>
     """,
@@ -61,19 +92,6 @@ with header_container:
 # Add a button to the row
     with col1:
         st.image("./assets/Frame 2.png", use_column_width=169.87)
-    with col2:
-        button1 = st.button("HOME")
-    with col3:
-        button2 = st.button("DETECTOR")
-    with col4:
-        button3 = st.button("ABOUT")
-
-    if button1:
-        subprocess.run(["streamlit", "run", "home.py"])
-    if button2:
-        subprocess.run(["streamlit", "run", "detector.py"])
-    if button3:
-        subprocess.run(["streamlit", "run", "about.py"])
 
     st.markdown(
         '<ul class="nav-links">'
@@ -81,15 +99,51 @@ with header_container:
         '</ul>',
         unsafe_allow_html=True,
     )
+st.sidebar.write("Navigation")
+page = st.sidebar.radio("Go to:", ["🏡Home", "🎯Detector", "ℹ️ About"])
 
-# Display an image below the header
-st.image("./Frame 6.png", use_column_width=True)
+# Define content for each "page"
+if page == "🏡Home":
+    st.title("Home Page")
+    # Display an image below the header
+    st.image("./Frame 12.png", use_column_width=True)
+
+
+elif page == "🎯Detector":
+    st.title("Detector")
+    
+    button1 = st.button("../")
+    vert_space = '<div style="padding: 50px 5px;"></div>'
+    st.markdown(vert_space, unsafe_allow_html=True)
+
+    wallet_address = st.text_input("Wallet Address", key="wallet_input", value="")
+    percentage = 95  # You can change this percentage value
+
+    # Set the text color based on the percentage
+    if percentage > 65:
+        text_color = 'red'
+    else:
+        text_color = 'green'
 
 
 
-# Input text box with custom style
-wallet_address = st.text_input("Wallet Address", key="wallet_input", value="")
+    if wallet_address:
+        if wallet_address in df2['Address'].unique():
+            st.write(f"Entered Wallet Address: {wallet_address}")
+            st.markdown('<div class="fira-code-font" style="text-align: center; font-size: 20px;">Dirt Score</div>', unsafe_allow_html=True)
+            st.write(f"<div class='center-content fira-code-font' style='color: {text_color}; font-size: 36px;'><b>{percentage}</b></div>", unsafe_allow_html=True)
 
 
-if wallet_address:
-    st.write(f"Entered Wallet Address: {wallet_address}")
+            df3 = df2.loc[df2['Address'] == wallet_address] 
+            st.write(df3)
+        else:
+            st.write(df2)
+
+elif page == "ℹ️ About":
+    st.title("About")
+    
+    st.image("./Frame 7.png", use_column_width=True)
+
+
+
+
